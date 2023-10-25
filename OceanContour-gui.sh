bcc78760736db0c5d08dc220821692fbbd9a2d79
Sidney <sbatchelder@whoi.edu>
2023-10-24 20:50:22 -0400

    added bash mode

--- OceanContour-gui.sh
+++ OceanContour-gui.sh
@@ -6,10 +6,15 @@ if [ ! -d "workspace/.meta/.projects" ]; then
     unzip "workspace-init.zip"
 fi
 
+CMD="OceanContour"
+if [ "$1" = "bash" ]; then
+    CMD="bash"
+fi
+
 docker run -h oceancontour-container \
     -v "$(pwd):/app/vol" \
     -v "$(pwd)/workspace:/app/workspace" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix" -e DISPLAY="$DISPLAY" \
-    --rm -it oceancontour OceanContour  #bash
+    --rm -it oceancontour $CMD
 
 
